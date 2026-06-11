4f30bafa9bbcfad92e8bede1d1dea65b1af77d9f
Raksa Adhinata <raksaadhinata@gmail.com>
2026-06-11 16:40:29 +0700

    Refactor silver_company_profiles table creation
    Refactor silver_company_profiles table creation to use CTE for better readability and deduplication.

--- src/silver.sql
+++ src/silver.sql
@@ -1,11 +1,17 @@
 CREATE OR REPLACE TABLE silver_company_profiles AS
-SELECT 
-    lpad(cik::VARCHAR, 10, '0') AS cik, -- Casting & Padding CIK
-    upper(name) AS company_name,        -- Normalisasi Nama
-    tickers[1]::VARCHAR AS primary_ticker,       -- Ambil ticker pertama dari list
-    exchanges[1]::VARCHAR AS primary_exchange,
-    sic::INTEGER AS sic_code,           -- Casting ke Integer
-    sicDescription AS industry,
-    entityType AS category,
-    fiscalYearEnd AS fiscal_year_end
-FROM read_json_auto('s3://config-elt-bucket/bronze/sec_data_*.json');
+WITH raw_extracted AS (
+    SELECT 
+        lpad(cik::VARCHAR, 10, '0') AS cik,          -- Casting & Padding CIK
+        upper(name) AS company_name,                 -- Normalisasi Nama
+        tickers[1]::VARCHAR AS primary_ticker,       -- Ambil ticker pertama
+        exchanges[1]::VARCHAR AS primary_exchange,
+        sic::INTEGER AS sic_code,                    -- Casting ke Integer
+        sicDescription AS industry,
+        entityType AS category,
+        fiscalYearEnd AS fiscal_year_end
+    FROM read_json_auto('s3://config-elt-bucket/bronze/sec_data_*.json')
+)
+SELECT * 
+FROM raw_extracted
+-- Membuang duplikasi: Jika ada CIK yang sama, ambil salah satu saja
+QUALIFY ROW_NUMBER() OVER (PARTITION BY cik ORDER BY company_name DESC) = 1;
