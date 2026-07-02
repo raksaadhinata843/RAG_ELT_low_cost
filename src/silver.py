2198fec7b9eef32e103d7fbff12852dbf5045f29
Raksa Adhinata <raksaadhinata@gmail.com>
2026-07-02 15:48:00 +0700

    Update silver.py

--- src/silver.py
+++ src/silver.py
@@ -1,7 +1,10 @@
 import boto3
 import duckdb
+import os
 
 def get_latest_data():
+    con = duckdb.connect(f'md:?md_token={os.environ["MD_TOKEN"]}')
+    
     s3 = boto3.client('s3')
     bucket = 'config-elt-bucket'
     prefix = 'bronze/'
@@ -14,7 +17,9 @@ def get_latest_data():
             folders.add(prefix_data['Prefix'])
     
     # 2. Ambil folder dengan tanggal terbaru (karena formatnya yyyy-mm-dd, string sort works!)
-    latest_folder = sorted(list(folders))[-1] # Contoh: 'bronze/2026-06-28/'
+    latest_folder = sorted(list(folders))[-1]
+    if not folders:
+        raise ValueError("No bronze folders found in S3. Run bronze.py first.")
     latest_path = f"s3://{bucket}/{latest_folder}*.json"
     
     print(f"Membaca data dari: {latest_path}")
@@ -38,5 +43,6 @@ def get_latest_data():
     SELECT * FROM raw_extracted
     QUALIFY ROW_NUMBER() OVER (PARTITION BY cik ORDER BY company_name DESC) = 1;
     '''
-    data = duckdb.query(query).to_df()
+    con.execute(query)
+    data = con.execute("SELECT * FROM silver_company_profiles").fetchdf()
     return data
